f968a80d67246b3d42e57bcca4f4cbdae39835b8
Hungtt22 <hungpro7227@gmail.com>
2025-10-19 19:17:11 +0700

    console

---
+++ src/console/console.js
@@ -0,0 +1,12 @@
+console.log("Hello, Console!");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+
+const name = "Alice";
+const age = 30;
+console.log(`Name: ${name}, Age: ${age}`);
+console.log("Name:", name, ", Age", age);
+console.log(
+    "%cCustom styled message",
+    "color: blue; font-size: 16px; font-weight: bold;"
+);
