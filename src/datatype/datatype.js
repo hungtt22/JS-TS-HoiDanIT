a96bdf327163b45746a0f14aa4a4e69d069dd37f
Hungtt22 <hungpro7227@gmail.com>
2025-10-19 18:29:16 +0700

    DataType

---
+++ src/datatype/datatype.js
@@ -0,0 +1,42 @@
+//Number
+const myAge = 21;
+const myGPA = 8.5;
+
+console.log("Age:", myAge);
+console.log("GPA:", myGPA);
+
+//String
+const myName = "John Doe";
+const myCity = "New York";
+
+console.log("Name:", myName);
+console.log("City:", myCity);
+
+//Boolean
+const isStudent = true;
+const isWorking = false;
+
+//Undefined
+let myJob;
+console.log("myJob:", myJob);
+
+//Null
+const myCar = null;
+console.log("myCar:", myCar);
+
+console.log(typeof myCar);
+console.log(typeof myJob);
+console.log(typeof isWorking);
+
+//Object
+const myDetails = {
+    name: "John Doe",
+    age: 21,
+    city: "New York",
+};
+console.log("Details:", myDetails);
+
+//Array
+const mySkills = ["JavaScript", "HTML", "CSS"];
+console.log("Skills:", mySkills);
+console.log("First Skill:", mySkills[0]);
